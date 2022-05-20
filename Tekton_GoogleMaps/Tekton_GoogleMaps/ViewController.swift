699b7b87f23e9b7f1050fb9b298dfea28bedb03b
Miguel Otero <miguel-otero-calderon@hotmail.com>
2022-05-20 14:30:39 -0500

    Access permisions

--- Tekton_GoogleMaps/Tekton_GoogleMaps/ViewController.swift
+++ Tekton_GoogleMaps/Tekton_GoogleMaps/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        GMSServices.provideAPIKey("AIzaSyAoSPIC5OEQcAC2CZG-k7RspDtn_GZNNoI")
     }
-
-
 }
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        let coordinate = location.coordinate
+        let camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
 
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude:coordinate.latitude, longitude: coordinate.longitude)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+}
